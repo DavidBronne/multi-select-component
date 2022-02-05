7c53c5062612511ac016add1dec0fcb4cc2ffa8d
David Bronne <davidbronne@gmail.com>
2022-02-05 21:33:39 +0100

    prevent space at beginning and end of option added

--- src/components/AddField.tsx
+++ src/components/AddField.tsx
@@ -7,8 +7,8 @@ interface PropsAddOption {
 const AddField:React.FC<PropsAddOption> = ( {addOption} ) => {
     const [option , setOption] = useState("");
 
-    const isDisabled = option.length ? false : true
-
+    const isDisabled = option.trim().length ? false : true
+console.log('option.trim().length :>> ', option.trim().length);
     const handleChange:React.ChangeEventHandler<HTMLInputElement> = (e) => {
         setOption(e.target.value);
     }
